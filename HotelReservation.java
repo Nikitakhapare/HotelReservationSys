b1c5f317b9bb9bc336f51562ef693509c92aa186
Nikita <nikitakhapare53@gmail.com>
2022-03-07 09:22:36 +0530

    Added:Welcome to Hotel Reservation

---
+++ HotelReservation.java
@@ -0,0 +1,9 @@
+package Bridgelabz.HotelReservationSystem;
+
+public class HotelSystem {
+
+public static void main(String[] args) {
+	System.out.println("Welcome to hotel Reservation System");
+
+	}
+}
