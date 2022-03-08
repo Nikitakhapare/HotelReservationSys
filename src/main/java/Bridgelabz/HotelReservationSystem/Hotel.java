5f94ca2eeb81f81f4cde2c4cc9afb4ffe557c6fa
Nikita <nikitakhapare53@gmail.com>
2022-03-08 19:07:00 +0530

    Added: WeekDays and WeekendDays in hotel reservation system

--- src/main/java/Bridgelabz/HotelReservationSystem/Hotel.java
+++ src/main/java/Bridgelabz/HotelReservationSystem/Hotel.java
@@ -4,9 +4,12 @@ public class Hotel {
 	private String hotelName;
 	private int regularCustomerRate;
 	private int totalRate;
-	public Hotel(String hotelName, int regularCustomerRate) {
+	private int weekDayRate;
+	private int weekendRate;
+	public Hotel(String hotelName, int weekDayRate,int weekendRate) {
 		this.hotelName = hotelName;
-		this.regularCustomerRate = regularCustomerRate;
+         this.weekDayRate=weekDayRate;
+        this.weekendRate=weekendRate;
 	}
 	public String getHotelName() {
 		return hotelName;
@@ -19,6 +22,18 @@ public class Hotel {
 	}
 	public void setRegularCustomerRate(int regularCustomerRate) {
 		this.regularCustomerRate = regularCustomerRate;
+	}
+	public int getweekDayRate() {
+		return getweekDayRate();
+	}
+	public void setWeekDayRate(int weekDayRates) {
+		this.weekDayRate = weekDayRates;
+	}
+	public int getWeekendRates() {
+		return weekendRate;
+	}
+	public void setWeekEndRates(int weekEndRates) {
+		this.weekendRate = weekEndRates;
 	}
 	public int getTotalRate() {
 		return totalRate;
--- src/test/java/Bridgelabz/HotelReservationSystem/AddNewHotelTest.java
+++ src/test/java/Bridgelabz/HotelReservationSystem/AddNewHotelTest.java
@@ -8,23 +8,22 @@ public class AddNewHotelTest {
 	@Test
 	public void toAddHotelHotel_Should_returnTrue() {
 		AddNewHotel hotelReservationSystem = new AddNewHotel();
-		Hotel hotelCheck = new Hotel("Bridgewood", 160);
-		Hotel hotelCheck1 = new Hotel("Lakewood", 110);
-		Hotel hotelCheck2 = new Hotel("Ridgewood", 220);
-	    boolean checkIfAdded =  hotelReservationSystem.addHotel(hotelCheck);
+		Hotel hotelCheck = new Hotel("Bridgewood", 150,90);
+		Hotel hotelCheck1 = new Hotel("Lakewood", 110,67);
+		Hotel hotelCheck2 = new Hotel("Ridgewood", 220,99);
+	    boolean checkIfAdded =  hotelReservationSystem.addHotel("Bridgewood", 150,90);
 		Assert.assertEquals(checkIfAdded, true);
 	}
 	 @Test
 	    public void testForcheapestHotelWithinADateRange() {
 	        AddNewHotel service = new AddNewHotel();
-	        Hotel hotel1 = new Hotel("Lakewood", 220);
-	        Hotel hotel2 = new Hotel("Bridgewood", 160);
-	        Hotel hotel3 = new Hotel("Ridgewood", 220);
-	        service.addHotel(hotel1);
-	        service.addHotel(hotel2);
-	        service.addHotel(hotel3);
+	        service.addHotel("Lakewood", 220,67);
+	        service.addHotel("Bridgewood", 160,98);
+	        service.addHotel("Ridgewood", 220,90);
 	        Hotel hotel = service.findCheapestHotel("10Sep2020","11Sep2020");
-            Assert.assertEquals("Lakewood", hotel.getHotelName());  
-	    }
+            Assert.assertEquals("Bridgewood", hotel.getHotelName());  
+	    
+		}
+	 
 	}
 
